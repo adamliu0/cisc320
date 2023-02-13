3c743fcb1686fad7e1fe9e6df8bee6e640661ab7
adamliu0 <adamliu@udel.edu>
2023-02-12 22:51:13 -0500

    done

--- lesson03/answer.py
+++ lesson03/answer.py
@@ -3,17 +3,21 @@ def sum_func(lines: list[str]) -> str:
     if (len <= 0) or (len == None):
         return "EMPTY"
     sum = 0
+    num_positive = 0
     for i in range(1, len+1):
         curr = int(lines[i])
         if curr == -999:
             break
         if curr >= 0:
             sum += curr
-    return sum
-        
+            num_positive += 1
+    if num_positive > 0:
+        return sum
+    return "EMPTY"
 
 if __name__ == "__main__":
     filename = input()
     with open(filename) as data_file:
         lines = data_file.readlines()
+    print(lines[0])
     print(sum_func(lines))
