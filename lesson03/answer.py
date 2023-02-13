21500f3361fcdd4beb7486b514f02b03b208033e
Adam Liu <74369851+adamliu0@users.noreply.github.com>
2023-02-12 21:07:34 -0500

    Create answer.py

---
+++ lesson03/answer.py
@@ -0,0 +1,19 @@
+def sum_func(lines: list[str]):
+    len = int(lines.readline())
+    if len <= 0 or len == None:
+        return "EMPTY"
+    sum = 0
+    for i in range(len):
+        curr = int(lines.readline())
+        if curr == -999:
+            break
+        if curr >= 0:
+            sum += curr
+    return sum
+        
+
+if __name__ == "__main__":
+    filename = input()
+    with open(filename) as data_file:
+        lines = data_file.readlines()
+    sum_func(lines)
